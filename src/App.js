2257cc6dade7d5226ba7c9ea17bb82a262b51457
Rhub-r404 <rense.vandewalle@gmail.com>
2025-02-01 14:29:31 +0100

    update3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import {HashRouter as Router} from "react-router-dom";
 import DecimalClockCanvas from "./DecimalClockCanvas";
-import Home from "./Home";
-import About from "./About";
+
 
 function App() {
   return (
